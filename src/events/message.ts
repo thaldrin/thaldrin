4d4dc780291033b67e0b92806edddf6765dac966
Lio Young <lio@werewolf.design>
2021-04-06 03:11:05 +0200

    get prefixes to 'work'

--- src/events/message.ts
+++ src/events/message.ts
@@ -3,6 +3,7 @@ import { Client, Collection, Message } from "discord.js";
 import Logger from "../utils/logger";
 import supabase from "../utils/database";
 import { Server, Usage } from "../utils/types";
+import config from "../../config";
 
 export = {
     name: "message",
@@ -10,17 +11,27 @@ export = {
         if (message.author.bot) return
         if (message.channel.type === "dm") return
 
-        let { data: server_data, error: server_error } = await supabase.from<Server>("servers").select().eq(`server_id`, message.guild.id).limit(1)
+        // ! Messages seen 
         let { data: usage_data, error: usage_error } = await supabase.from<Usage>("usage").select().eq('name', "message")
-
         if (usage_data?.length === 0) {
             let { data: a, error: b } = await supabase.from<Usage>("usage").insert({ name: "message", type: "event" }).select()
             usage_data = a
         }
-
         let { data: updated_usage_data, error } = await supabase.from<Usage>('usage').update({ amount: usage_data[0].amount + 1 }).select().eq("name", "message")
-        // console.log(message.guild?.name, server_data)
-        console.log("usage log", usage_data)
+        // ! Messages seen
+
+        // ? Getting Server config
+        let { data: server_data, error: server_error } = await supabase.from<Server>("servers").select().eq(`server_id`, message.guild.id).limit(1)
+        if (server_data?.length === 0) {
+            let { data: c, error: d } = await supabase.from<Server>('servers').insert({
+                server_id: message.guild?.id
+            })
+        }
+        // ! Prefix
+        let PrefixArray: string[] = [...config.variables.prefix, [server_data[0].prefix]].flat(Infinity)
+
+        console.log(`Prefixes for ${message.guild.name} | `, PrefixArray)
+
 
 
     }
