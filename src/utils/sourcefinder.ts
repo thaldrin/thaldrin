a9e9e19802e829d454cf3683ccce211cec9181e1
Lio Melio <lio@werewolf.design>
2021-04-07 13:18:07 +0200

    remove discord.js import

--- src/utils/sourcefinder.ts
+++ src/utils/sourcefinder.ts
@@ -1,5 +1,4 @@
 import Sourcefinder from "@thaldrin/sourcefinder";
-import { Message } from "discord.js";
 import config from "../../config";
 const sourcefinder = new Sourcefinder(`Thaldrin/v${config.pkg.version} (t8.pm/bot)`)
 
