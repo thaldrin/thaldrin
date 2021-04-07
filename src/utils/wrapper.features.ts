f2aa391651474eea6fa31e215bad299e4b669d18
Lio Melio <lio@werewolf.design>
2021-04-07 13:19:23 +0200

    start working on feature checks

--- src/utils/wrapper.features.ts
+++ src/utils/wrapper.features.ts
@@ -7,8 +7,21 @@ let SL = /(nosl|no-?short(link(s|ing)?)?)/gmi
 let SF = /(nosf|no-?source(find(er|ing)?)?)/gmi
 
 
+async function check(content: string, feature: string) {
+    switch (feature) {
+        case 'sl':
+            console.log(content.match(SL))
+            break;
+
+        default:
+            break;
+    }
+}
+
+
 
 export default async function Shortlink(message: Message, setting: boolean) {
     if (!setting) return;
+    let enabled = await check(message.content, 'sl')
 
 }
