7721b4f67baed85b3d2f90e7d01bc43877916445
Lio Young <lio@werewolf.design>
2021-04-09 20:44:07 +0200

    move devs to variables and add types

--- variables.ts
+++ variables.ts
@@ -1,4 +1,18 @@
 export default {
+    /**
+    * Name of the Bot in Logs 
+    */
     name: "Panthera",
+    /**
+     * Default Prefixes 
+     */
     prefix: ['thal', "<@434662676547764244>", "<@!434662676547764244>"],
+    /**
+     * Developers of the Bot
+     */
+    developers: [
+        {
+            id: "318044130796109825" // Lio
+        }
+    ]
 }
