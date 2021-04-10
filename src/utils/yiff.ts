80574888d1d90c34819c3cc61e4caf797fcb3bd2
Lio Young <lio@werewolf.design>
2021-04-10 23:26:35 +0200

    make the yiff useragent use name set in vars

--- src/utils/yiff.ts
+++ src/utils/yiff.ts
@@ -2,7 +2,7 @@ import Yiff from 'yiff'
 import config from '../../config'
 import pkg from '../../package.json'
 let yiff = new Yiff({
-    useragent: `Thaldrin/v${pkg.version} (t8.pm/bot)`,
+    useragent: `${config.variables.name}/v${pkg.version} (t8.pm/bot)`,
     apikey: {
         sheri: config.apis.sheri,
         yiffrest: config.apis.yiffrest
