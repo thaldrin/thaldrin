6989537da99ce3ea8ed6e4d7bbe3fc4b2cfa84fa
Lio Young <lio@werewolf.design>
2021-04-10 23:26:07 +0200

    add shard count to startup

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import Logger from "./src/utils/logger";
 Util.fetchRecommendedShards(config.token).then((count) => {
     // console.log(`Starting ${config.variables.name}`)
     Logger.info({
-        message: `Starting ${config.variables.name}....`,
+        message: `Starting ${config.variables.name} with ${count} Shards`,
         type: "event:start"
     })
     new client(config, count)
