1f6bf93fe4b098dc96749fbc691e83b081dd417b
Lio Young <lio@werewolf.design>
2021-04-28 03:34:16 +0200

    fix bird command

--- src/modules/images/birb.ts
+++ src/modules/images/birb.ts
@@ -3,17 +3,18 @@ import Command from '../../handler/structures/Command';
 import { Context } from '../../utils/types';
 import { MessageEmbed } from 'discord.js';
 
-export = class Cat extends Command {
+export = class Birb extends Command {
     constructor() {
         super({
-            name: "cat",
-            description: "Show a Cat",
+            name: "bird",
+            description: "Show a Bird",
+            aliases: ["birbs", "birb", "birds"],
             cooldown: 1,
         })
     }
 
     async command(ctx: Context) {
-        let image = await yiff.shibe("cats", 1)
+        let image = await yiff.shibe("birds", 1)
         let provider = "shibe.online"
         if (ctx.settings.embeds) {
             let Cat = new MessageEmbed()
