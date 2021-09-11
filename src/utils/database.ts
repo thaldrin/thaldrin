6145477a66a161c31a89c0324c055af4f7c808c9
Lio Young <lio@werewolf.design>
2021-09-11 13:38:09 +0200

    move database stufff to modulus

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,9 +1,16 @@
-import { createClient } from '@supabase/supabase-js'
-import config from '../../config'
-import { Server } from "./types";
+// import { createClient } from '@supabase/supabase-js'
+// import config from '../../config'
+// import { Server } from "./types";
 
-const supabase = createClient(config.supabase.url, config.supabase.key)
+// const supabase = createClient(config.supabase.url, config.supabase.key)
 
 
 
-export default supabase
+// export default supabase
+
+import Modulus from "../utils/modulus/src/index";
+
+const modulus = new Modulus()
+
+
+export default modulus
